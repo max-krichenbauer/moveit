a5643f425d16c65125ac239c30f613e0ebf9c717
Max Krichenbauer <mail@makx.net>
2020-01-07 21:18:38 +0900

    Edited previous commit for better error reporting.

--- travis_coverity.sh
+++ travis_coverity.sh
@@ -322,7 +322,7 @@ function build_workspace() {
    TOOL_DIR=`find $TOOL_BASE -type d -name 'cov-analysis*'`
    export PATH=$TOOL_DIR/bin:$PATH
    ARCH=`arch`
-   sudo ln -s $TOOL_DIR/bin/libcapture-linux64-$ARCH.so $TOOL_DIR/bin/libcapture-linux64-\\\${PLATFORM}.so
+   ln -v -s $TOOL_DIR/bin/libcapture-linux64-$ARCH.so $TOOL_DIR/bin/libcapture-linux64-\\\${PLATFORM}.so
    echo -e "\033[33;1mConfiguring Coverity Scan Analysis Tool...\033[0m"
    cov-configure --clang
    echo -e "\033[33;1mRunning Coverity Scan Analysis Tool...\033[0m"
